2d6143b09869db97befa2b46157baf87e9d828cc
DGil <55130851+gh33ls0n@users.noreply.github.com>
2019-09-10 18:28:37 +0200

    Wersja 1

---
+++ SolutionEmerytura/ConsoleAppEmerytura/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEmerytura
+{
+    class Program
+    {
+        const int wiekEmerytalny = 67;
+        const int rokprzed = wiekEmerytalny - 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja Emerytura");
+
+            Console.Write("Podaj imię: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}!");
+
+            Console.Write("Podaj wiek: ");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek < 67)
+            {
+                Console.WriteLine($"Zostało Ci {wiekEmerytalny - wiek} lat do emerytury");
+
+            }
+            else if (wiek == rokprzed)
+                {
+                    Console.WriteLine($"Zostało Ci {rokprzed} rok do emerytury");
+                }
+            
+            else
+            {
+                Console.WriteLine("Jesteś emerytem.");
+            }
+            //Console.ReadKey(); // oczekuje na naciśnięcie dowolnego klawisza
+            
+        }
+    }
+}
